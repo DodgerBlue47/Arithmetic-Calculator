76043386b88ada0437859da9f7176573dd06acb5
DodgerBlue47 <145989581+DodgerBlue47@users.noreply.github.com>
2024-02-06 01:51:01 +0800

    Update com

---
+++ sources/com.dodgerblue.calculator/src/java/com/shashank/sony/fancytoastlib/R.java
@@ -0,0 +1,81 @@
+package com.shashank.sony.fancytoastlib;
+
+public final class R {
+
+    public static final class color {
+        public static final int colorAccent = 2130771968;
+        public static final int colorControlHighlight = 2130771969;
+        public static final int colorControlNormal = 2130771970;
+        public static final int colorPrimary = 2130771971;
+        public static final int colorPrimaryDark = 2130771972;
+    }
+
+    public static final class drawable {
+        public static final int androidicon = 2130837504;
+        public static final int app_icon = 2130837505;
+        public static final int confusing_shape = 2130837506;
+        public static final int default_image = 2130837507;
+        public static final int default_shape = 2130837508;
+        public static final int error_shape = 2130837509;
+        public static final int ic_check_black_24dp = 2130837510;
+        public static final int ic_clear_black_24dp = 2130837511;
+        public static final int ic_info_outline_black_24dp = 2130837512;
+        public static final int ic_pan_tool_black_24dp = 2130837513;
+        public static final int ic_refresh_black_24dp = 2130837514;
+        public static final int info_shape = 2130837515;
+        public static final int success_shape = 2130837516;
+        public static final int warning_shape = 2130837517;
+    }
+
+    public static final class id {
+        public static final int Background = 2130903040;
+        public static final int Backspace = 2130903041;
+        public static final int Clear = 2130903042;
+        public static final int Devide = 2130903043;
+        public static final int Equal = 2130903044;
+        public static final int Five = 2130903045;
+        public static final int Four = 2130903046;
+        public static final int Minus = 2130903047;
+        public static final int Nine = 2130903048;
+        public static final int One = 2130903049;
+        public static final int Plus = 2130903050;
+        public static final int Result = 2130903051;
+        public static final int Seven = 2130903052;
+        public static final int Six = 2130903053;
+        public static final int Three = 2130903054;
+        public static final int Times = 2130903055;
+        public static final int Two = 2130903056;
+        public static final int Zero = 2130903057;
+        public static final int eight = 2130903058;
+        public static final int imageView4 = 2130903059;
+        public static final int linear1 = 2130903060;
+        public static final int linear2 = 2130903061;
+        public static final int linear3 = 2130903062;
+        public static final int linear4 = 2130903063;
+        public static final int linear5 = 2130903064;
+        public static final int linear6 = 2130903065;
+        public static final int linear7 = 2130903066;
+        public static final int non = 2130903067;
+        public static final int nonb = 2130903068;
+        public static final int nonnn = 2130903069;
+        public static final int toast_icon = 2130903070;
+        public static final int toast_text = 2130903071;
+        public static final int toast_type = 2130903072;
+    }
+
+    public static final class layout {
+        public static final int fancytoast_layout = 2130968576;
+        public static final int main = 2130968577;
+    }
+
+    public static final class string {
+        public static final int app_name = 2131034112;
+    }
+
+    public static final class style {
+        public static final int AppTheme = 2131099648;
+        public static final int FullScreen = 2131099649;
+        public static final int NoActionBar = 2131099650;
+        public static final int NoStatusBar = 2131099651;
+    }
+}
