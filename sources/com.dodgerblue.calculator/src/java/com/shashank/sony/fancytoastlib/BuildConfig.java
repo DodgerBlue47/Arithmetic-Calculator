c1064db61e5b9cdc71fe1f4a7ebfc1f02cf2dfbe
DodgerBlue47 <145989581+DodgerBlue47@users.noreply.github.com>
2024-02-06 01:51:49 +0800

    Update fancytoastlib

---
+++ sources/com.dodgerblue.calculator/src/java/com/shashank/sony/fancytoastlib/BuildConfig.java
@@ -0,0 +1,7 @@
+package com.shashank.sony.fancytoastlib;
+
+public final class BuildConfig {
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final String LIBRARY_PACKAGE_NAME = "com.shashank.sony.fancytoastlib";
+}
